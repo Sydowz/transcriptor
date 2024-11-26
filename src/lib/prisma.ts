ab76c318464fb67ac68079f764ff2d14975cdcb5
Sydow <jonitossydow1@gmail.com>
2024-11-26 18:26:27 -0300

    asd

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,3 +1,23 @@
 import { PrismaClient } from "@prisma/client";
 
-export const prisma = new PrismaClient();
+const prismaClientSingleton = () => {
+  return new PrismaClient({
+    datasources: {
+      db: {
+        url: process.env.DATABASE_URL
+      }
+    },
+    log: ['error', 'warn'],
+    // Aumentar os timeouts e configurar reconexão
+    connectionTimeout: 60000, // 60 segundos
+    requestTimeout: 60000, // 60 segundos
+  })
+}
+
+declare global {
+  var prisma: undefined | ReturnType<typeof prismaClientSingleton>
+}
+
+export const prisma = globalThis.prisma ?? prismaClientSingleton()
+
+if (process.env.NODE_ENV !== 'production') globalThis.prisma = prisma
